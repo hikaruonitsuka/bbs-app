794b06c97184c5611af24423f8780c37ddf16cb7
hikaruonitsuka <linlin.work071109@gmail.com>
2024-03-16 11:50:53 +0900

    [add]:TailwindCSSの追加

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <header>ヘッダー</header>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <header className="m-4 p-4 text-3xl font-extrabold">ヘッダー</header>
+    </div>
+  );
+}
+
+export default App;
